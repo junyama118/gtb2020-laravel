ed85b2da79678845e0e5fce9d8fa68ac4ff75c1f
junyamaroot <junyama@example.com>
2020-05-28 16:37:54 +0900

    inputaccountinfoを書き換えた

--- resources/views/layouts/inputaccountinfo.blade.php
+++ resources/views/layouts/inputaccountinfo.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet"  href="mobile_inputaccountinfo.css">
+        <link rel="stylesheet" media="(max-width: 900px)" href="{{ asset('css/mobile_inputaccountinfo.css') }}">
         <!--<link rel="srylesheet" media="(min-widtn: 900px)" href="pc_createaccount.css">-->
         <title>口座情報入力</title>
     </head>
@@ -18,26 +18,28 @@
 
             <div class="inputaccountinfo-container">
 
-                <form action="#" method="post">
+                <form action="/users" method="post">
+                    {{ csrf_field() }}
                     <div class="questiontitle title_branchnumber">
-                        <label for="askbranchnumber">支店番号:</label>
+                        <label for="branckCode">支店番号:</label>
                     </div>
                     <div>
-                        <input type="number" id="askbranchnumber" required class="questioncolum branchnumbercolum">
+                        <input type="number" name="branckCode" required class="questioncolum branchnumbercolum">
                     </div>
+
                     <div class="questiontitle title_accountnumber">
-                        <label for="addaccountnumber">口座番号:</label>
+                        <label for="accountNumber">口座番号:</label>
                     </div>
                     <div>
-                        <input type="number" id="addaccountnumber" required class="questioncolum accountnumbercolum">
+                        <input type="number" name="accountNumber" required class="questioncolum accountnumbercolum">
                     </div>
+
                     <div class="questiontitle title_token">
-                        <label for="addtoken">トークン:</label>
+                        <label for="token">トークン:</label>
                     </div>
                     <div>
-                        <input type="text" id="addtoken" required class="questioncolum tokencolum">
+                        <input type="text" name="token" required class="questioncolum tokencolum">
                     </div>
-                    
 
                     <input type="submit" name="submitvalue" value="口座情報登録" class="submitbottom">
 
