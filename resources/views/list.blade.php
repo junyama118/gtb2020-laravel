ef072281cf31f4d9e0ae5a682503759aa1c5647a
junyamaroot <junyama@example.com>
2020-05-29 16:28:27 +0900

    [fix]IDが必要ないので消した

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -16,7 +16,6 @@
                 <table class="table table-striped">
                 @foreach($users as $user)
                     <tr>
-                        <td>{{$user->id}}</td>
                         <td>{{$user->name}}</td>
                         <td>{{$user->email}}</td>
                         <td>
